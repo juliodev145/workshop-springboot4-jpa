a67c59a0821ac44b44e33138b3a1412dbafa816f
juliodev145 <juliocezar.dev10@gmail.com>
2026-09-20 19:24:04 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,10 @@ public class User implements Serializable { //Serializable serve para transforma
 	private String email;
 	private String phone;
 	private String password;
+	
+	//acrescentando associação (1 - N)
+	@OneToMany(mappedBy = "client") 
+	private List<Order> orders = new ArrayList<>();
 
 	// é obrigado pelo framework
 	public User() {
@@ -74,6 +81,10 @@ public class User implements Serializable { //Serializable serve para transforma
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
